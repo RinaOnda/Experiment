9f86a76780b52a57da5751136e821fd70e3f5363
Rina Onda <rinaonda@RinanoMBP.psi.ch>
2020-03-03 11:29:15 +0100

    minor change

--- funcgen/turn_on.py
+++ funcgen/turn_on.py
@@ -2,19 +2,19 @@
 
 import sys,subprocess
 
-ip_address = '129.129.228.107'
+ip_address = 'ip address of the function generator'
 
 
 if len(sys.argv)==1:
-        ch_list= ['1','2']
+    ch_list= ['1','2']
 elif len(sys.argv)==2:
-        ch_list = [sys.argv[1]]
+    ch_list = [sys.argv[1]]
 else:
-        print 'Usage: ./turn_on.py'
-        print 'Usage: ./turn_on.py [Ch]'
-        exit(1)
+    print('Usage: ./turn_on.py')
+    print('Usage: ./turn_on.py [ch]')
+    exit(1)
 
 for ch in ch_list:
-	cmd = '~/tek/utils/tek_afg/tek_afg -i %s E:%s:ON' % (ip_address,ch)
-	print cmd
-	subprocess.check_output(cmd, shell=True)
+    cmd = '~/tek/utils/tek_afg/tek_afg -i %s E:%s:ON' % (ip_address,ch)
+    print(cmd)
+    subprocess.check_output(cmd, shell=True)
